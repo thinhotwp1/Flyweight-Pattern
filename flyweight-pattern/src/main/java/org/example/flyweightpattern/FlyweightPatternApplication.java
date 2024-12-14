9be21c95329a1a253c83b6e5bca0679198298d9e
Jame Lee <thinhld0019@gmail.com>
2024-12-14 11:11:48 +0700

    'init_spring_flyweight_pattern'

---
+++ flyweight-pattern/src/main/java/org/example/flyweightpattern/FlyweightPatternApplication.java
@@ -0,0 +1,13 @@
+package org.example.flyweightpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyweightPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyweightPatternApplication.class, args);
+	}
+
+}
---
+++ flyweight-pattern/src/test/java/org/example/flyweightpattern/FlyweightPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flyweightpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyweightPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
